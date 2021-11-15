04668d0bdac13d5bc2660d535a1a8e4ac354d870
yvargasp1 <yavargasp1@ucentral.edu.co>
2021-11-15 14:58:05 -0500

    Se agrego la funcionalidad : Se separo Shop cart deudas y ventas

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,11 +5,12 @@ import {
   createProducts,
   getPhoto,
   getShopCar,
+  getShopCarDebt,
 } from '../controllers/product.controller'
 
 router.post('/create-products', [verifyToken, isSeller], createProducts)
 
 router.get('/product/:id', getPhoto)
 router.get('/shop-car', [verifyToken, isSeller],getShopCar)
-
+router.get('/shop-car-debt', [verifyToken, isSeller], getShopCarDebt)
 export default router
